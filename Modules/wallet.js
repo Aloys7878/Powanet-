4fa99dde772f47dcc2c255c8a83ae6407bb77e49
POWANET <nimbonaaloys55@gmail.com>
2025-11-09 22:03:03 +0200

    Added Quantum Login + Wallet + EcoImpact + Web3 modules (v3 Final Setup)
    modules/wallet.js

---
+++ Modules/wallet.js
@@ -0,0 +1,5 @@
+export const getBalances = () => ({
+  testnet: { pi: 3.14159, powa: 1000, usd: 314159 },
+  mainnet: { pi: 314.159, powa: 25000, usd: 31415900 },
+  mobileMoney: { mtn: 500, airtel: 450 },
+});
